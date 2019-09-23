2ab0822623b7ce2ba45d88b12c539e3d7c614c35
stonemannen <stonemannen1@gmail.com>
2019-09-23 14:47:13 +0200

    web server

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const http = require('http');
 const WebSocket = require('ws');
  
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n');
+});
 const wss = new WebSocket.Server({ server });
  
 wss.on('connection', function connection(ws) {
