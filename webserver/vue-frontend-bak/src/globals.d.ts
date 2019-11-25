7a656becc9d1a781f1a52c6c3aa6ca22d2856906
HACKING-STATION\gunhaxxor <gunnar.oledal@gmail.com>
2019-11-25 17:54:03 +0100

    Removed old version of vue-frontend

--- webserver/vue-frontend-bak/src/globals.d.ts
+++
@@ -1,1 +0,0 @@
-declare var pixelStates: number[];
--- webserver/vue-frontend-bak/vue.config.js
+++
@@ -1,6 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  // Changed so that the build step builds into the node server's public folder
-  outputDir: path.resolve(__dirname, "../public")
-};
